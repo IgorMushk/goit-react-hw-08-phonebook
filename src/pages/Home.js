cb30aeb715f83260bb029d66d2334b4298fa18ff
IgorMushk <igor.mushketik@gmail.com>
2023-10-19 17:50:09 +0300

    upd pages\Homeюоі

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,10 +16,7 @@ const styles = {
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>
-          Contacts manager welcome page{' '}
-          <span role="img" aria-label="Greeting icon">
-            💁‍♀️
-          </span>
+          Contacts manager welcome page
         </h1>
       </div>
     );
