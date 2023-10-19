32f96b4dd53fc56684b6715fd9f37879631c4ee8
IgorMushk <igor.mushketik@gmail.com>
2023-10-20 00:11:29 +0300

    Add PrivateRoute + RestrictedRoute -working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-
 import { useDispatch } from 'react-redux';
 import { lazy, useEffect } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,6 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { useAuth } from 'hooks';
 import { refreshUser } from 'redux/auth/operations';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -26,10 +27,30 @@ export function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
       </Route>
     </Routes>
   );
 }
+
+//RegisterPage
