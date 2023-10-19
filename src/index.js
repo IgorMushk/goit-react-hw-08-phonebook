62e99ad1e4c5035abfce5e9b244e8950356f5134
IgorMushk <igor.mushketik@gmail.com>
2023-10-19 14:23:21 +0300

    Add BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from "./redux/store";
+import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>  
-      <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/react-mod08-users-todo">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
