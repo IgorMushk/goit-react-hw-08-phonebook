350425afdf38c19e00df21737c75cfe7062c0ad3
IgorMushk <igor.mushketik@gmail.com>
2023-10-20 12:22:29 +0300

    Add Footer  +  <main>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,25 @@ import { Outlet } from 'react-router-dom';
 // import { Toaster } from 'react-hot-toast';
 import { AppBar } from './AppBar/AppBar';
 import { Suspense } from 'react';
+import { Footer, FooterText } from './Layout.styled';
 
 export const Layout = () => {
   return (
-    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    // <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    <>
       <AppBar />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+      <main>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </main>
       {/* <Toaster position="top-right" reverseOrder={false} /> */}
-    </div>
+      <Footer>
+        <FooterText>
+          &copy; {new Date().getFullYear()} Phonebook manager
+        </FooterText>
+      </Footer>
+    </>
+    // </div>
   );
 };
---
+++ src/components/Layout.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  left: 0px;
+  bottom: 0px;
+  width: 100%;
+  background-color: rgb(25, 118, 210);
+  color: rgb(255, 255, 255);
+  border-top: 2px solid rgba(22, 8, 214, 0.2);
+`;
+
+const FooterText = styled.p`
+  text-align: center;
+  padding: 10px 0;
+  margin: 0;
+`;
+
+export { Footer, FooterText };
